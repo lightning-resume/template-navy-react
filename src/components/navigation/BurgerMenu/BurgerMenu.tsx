d79497c6061763bfaafc44b1df075836d3162b96
jose <joselchecaexpo@gmail.com>
2021-08-20 13:08:19 +0100

    test(#4): add test to burgermenu

--- src/components/navigation/BurgerMenu/BurgerMenu.tsx
+++ src/components/navigation/BurgerMenu/BurgerMenu.tsx
@@ -1,8 +1,12 @@
 import './BurgerMenu.scss'
 
-export const BurgerMenu = ({ isActive, onClick }: { isActive: boolean; onClick: () => void }) => {
+export const BurgerMenu = ({ isActive, onClick }: { isActive?: boolean; onClick: () => void }) => {
   return (
-    <div onClick={onClick} className={['burger-container', isActive ? 'active' : 'no-active'].join(' ')}>
+    <div
+      aria-label="navigation menu"
+      onClick={onClick}
+      className={['burger-container', isActive ? 'active' : 'no-active'].join(' ')}
+    >
       <div className="burger-line first" />
       <div className="burger-line second" />
       <div className="burger-line third" />
